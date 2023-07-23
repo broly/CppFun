598a1bbd33e0f0ef2bc6dc2445b017b4b9e28f62
Artem Selivanov <2197187+broly@users.noreply.github.com>
2023-07-23 12:40:24 +0300

    Update DevilProperty#2.h

--- C# Properties/DevilProperty#2.h
+++ C# Properties/DevilProperty#2.h
@@ -1,5 +1,5 @@
 // github.com/broly/CppFun
-// This holder-less C# property implementation (v.2)
+// This is holder-less C# property implementation (v.2)
 // It gives possibility (on several compilers) to fully get rid of presense of this property in memory
 // On compiler like MSVC it still has size, but as little as possible
 // WARNING: DO NOT USE IT IN YOUR CODE! It is not compatible with non-standard class layouts
